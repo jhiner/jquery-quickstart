8d01d9e3d2e6be06dcd869d2fff84376b646f230
jhiner <hinerman@gmail.com>
2016-10-13 11:07:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# jquery-quickstart
+
+This is a prototype jQuery quickstart project to demonstrate new OAuth as a service features with Auth0.
+
+## Setup
+
+Rename `auth0-variables.js.example` to `auth0-variables.js` and make sure that you have AUTH0_CLIENT_ID, AUTH0_DOMAIN, and AUTH0_CALLBACK_URL defined. Also, make sure to add the callback URL (for example `http://localhost:8080/`) in the **Allowed Callback URLs** section.
+
+## Running the example
+
+In order to run the example you need to just start a server. What we suggest is doing the following:
+
+1. Install node
+2. run `npm install -g http-server`
+3. run `http-server` in the directory of the project.
+
+Go to `http://localhost:8080` and you'll see the app running :).
+
---
+++ app.js
@@ -0,0 +1,84 @@
+var auth0 = new Auth0({
+  domain:       AUTH0_DOMAIN,
+  clientID:     AUTH0_CLIENT_ID,
+  callbackURL:  AUTH0_CALLBACK_URL
+});
+
+$(document).ready(function() {
+  
+  // utility functions
+  // clear local storage upon logout
+  var logout = function() {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('access_token');
+    window.location.href = "/";
+  };
+
+  // app functionality
+
+  // check hash for access_token, id_token
+  var result = auth0.parseHash(window.location.hash);
+
+  // check if we are calling back from auth0 with access_token
+  if (result && result.idToken) {
+    $('#msg').text('Loading profile...');
+    $('#msg').show();
+    $('#btn-login').hide();
+    
+    localStorage.setItem('access_token', result.accessToken);
+    localStorage.setItem('id_token', result.idToken);
+    
+    auth0.getProfile(result.idToken, function (err, profile) {
+        $('.nickname').text(profile.nickname);
+        $('#login-link').hide();
+        $('.avatar').attr('src', profile.picture).show();
+        $('.btn-logout').show();
+        $('#profile').show();
+        $('#msg').hide();
+      });
+      // If offline_access was a requested scope
+      // You can grab the result.refresh_token here
+
+  } else if (result && result.error) {
+    console.log('error: ' + result.error);
+  }
+
+  $('#api-call').click(function(e) {
+    // invoke api securely
+    var accessToken = localStorage.getItem('accessToken');
+    fetch('http://todoapi2.api/authorized', {
+      method: 'get',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Authorization' : 'Bearer ' + accessToken
+      })
+    }).then(function(response) {
+      response.text().then(function(t) {
+        if (response.status !== 200) {
+          console.log('error');
+          return;
+        }
+        $('#msg').text('API Response: ' + JSON.stringify(JSON.parse(t)));
+        $('#msg').show();
+      })
+    }).catch(function(err) {
+      console.log(err);
+    });
+  });
+
+  $('.btn-logout').click(function(e) {
+    e.preventDefault();
+    logout();
+  });
+
+  $('#btn-login').click(function(e) {
+    auth0.login({
+      responseType: 'id_token token',
+      scope: 'openid profile read:todo',
+      audience: 'http://todoapi2.api',
+      prompt: 'consent',
+      sso: false
+    });
+  });
+
+});
